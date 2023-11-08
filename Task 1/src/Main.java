1fb4d5142976dea55f00e4a3876b8ea23359eddf
Кристина <dubininivan1990@gmail.com>
2023-11-08 13:28:30 +0400

    Выполнено задание

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
+        Phone ph1 = new Phone();
+        Phone ph2 = new Phone(8_927_785_64_78L, "Samsung", 15.5);
 
+        ph1.receiveCall("Pavel", 8_917_645_12_31L);
+        ph2.receiveCall("Robert");
+    }
+}
+
+class Phone {
+
+    long phoneNumber;
+    String modelPhone;
+    double weightPhone;
+    static String makeCountry;
+
+    Phone() {
+
+    }
+
+    public Phone(long phoneNumber, String modelPhone, double weightPhone) {
+        this.phoneNumber = phoneNumber;
+        this.modelPhone = modelPhone;
+        this.weightPhone = weightPhone;
+    }
+
+    void receiveCall(String name) {
+        System.out.println("Вам звонит " + name);
+    }
+
+    void receiveCall(String name, long number) {
+        receiveCall(name);
+        System.out.println("Номер: " + number);
     }
 }
